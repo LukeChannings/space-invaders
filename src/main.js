6da03a5fc10acf55e0e4931dc5451d0a4c52c083
Luke Channings <luke@channings.me>
2016-04-10 17:22:00 +0100

    Implement projectiles.

--- src/main.js
+++ src/main.js
@@ -12,26 +12,38 @@ import {
   fps,
 } from './signals'
 
+import {
+  HEIGHT,
+  MIDDLE,
+  CANNON_WIDTH,
+  CANNON_HEIGHT,
+  FRAMES_PER_SECOND,
+} from './constants'
+
 const {
   max,
   min,
 } = Math
 
-export default (persistedState) => {
+export default () => {
   const cannon = {
-    x: (window.innerWidth / 2) - 25,
+    x: MIDDLE - CANNON_WIDTH / 2,
     y: 0,
     vx: 0,
     vy: 0
   }
 
-  const initialState = persistedState || {
+  const projectiles = []
+
+  const initialState = {
     cannon,
+    projectiles,
   }
 
   const update = (state, [key, Δ]) => {
     return [
       move(key),
+      createProjectiles(key),
       physics(Δ),
     ].reduce((context, f) => f(context), state)
   }
@@ -46,27 +58,55 @@ export default (persistedState) => {
     }
   }
 
-  const physics = (Δ) => (state) => {
-    return {
-      ...state,
-      cannon: {
-        ...state.cannon,
-        x: min(max(0, state.cannon.x + Δ * state.cannon.vx), window.innerWidth - 50),
-      },
+  const createProjectiles = (key) => (state) => {
+    if (key.y > 0 && state.projectiles.length < 2) {
+      return {
+        ...state,
+        projectiles: state.projectiles.concat({
+          y: CANNON_HEIGHT,
+          x: state.cannon.x + CANNON_WIDTH / 2,
+        })
+      }
+    } else {
+      return state
     }
   }
 
-  const view = ([[windowWidth, windowHeight], {cannon}]) =>
-  <div className={`game`} style={`width: ${windowWidth}px; height: ${windowHeight}px`}>
-    <div className={`cannon`} style={`left: ${cannon.x}px; bottom: ${cannon.y}px`}></div>
-  </div>
+  const physics = (Δ) => (state) => {
+    const projectiles = state.projectiles.reduce((ps, {x, y}) => {
+      const newY = y + 20
+      if (newY > HEIGHT) {
+        return ps
+      } else {
+        return ps.concat({ x, y: newY })
+      }
+    }, [])
 
-  const delta = fps(30).map((t) => t / 20)
+    const cannon = {
+      ...state.cannon,
+      x: min(max(0, state.cannon.x + Δ * state.cannon.vx), window.innerWidth - 50),
+    }
+
+    return {
+      ...state,
+      cannon,
+      projectiles,
+    }
+  }
+
+  const view = ([[windowWidth, windowHeight], {cannon, projectiles}]) =>
+    <div className={`game`} style={`width: ${windowWidth}px; height: ${windowHeight}px`}>
+      <div className={`cannon`} style={`left: ${cannon.x}px; bottom: ${cannon.y}px`}></div>
+      {projectiles.map(({x, y}) =>
+          <div className={`projectile`} style={{ left: `${x}px`, bottom: `${y}px` }}></div>)}
+    </div>
+
+  const delta = fps(FRAMES_PER_SECOND).map((t) => t / 20)
 
   const input =
-  arrow$
-  .sampledBy(delta)
-  .zip(delta)
+    arrow$
+      .sampledBy(delta)
+      .zip(delta)
 
   const model$ = input.scan(update, initialState)
 
